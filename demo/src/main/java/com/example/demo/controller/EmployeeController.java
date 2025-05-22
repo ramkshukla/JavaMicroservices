ef55a12fd3ff91bd7a7c25682085c4bbb41d2701
ramkshukla <ramkumarshukla00000@gmail.com>
2025-05-23 01:32:56 +0530

    added micro services

---
+++ demo/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+
+
+import com.example.demo.entity.Employee;
+import com.example.demo.response.EmployeeResponse;
+import com.example.demo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Integer id) {
+        return employeeService.getEmployeeById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public java.util.List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
---
+++ demo/src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,27 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Optional<Employee> getEmployeeById(Integer id) {
+        return employeeRepository.findById(id);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
